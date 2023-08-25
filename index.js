05f4cbace2757a64a842d2ae38551191b8151267
aprimanfikri <aprmnfkr@gmail.com>
2023-08-25 22:54:34 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// 1. contoh array
+const contohArray = [1, "imam", 28, 2, "Ale", 20];
+// console.log(arr)
+
+// console.log(`ini index kedua dari array arr ${arr[1]}`)
+
+for (let i = 0; i < contohArray.length; i++) {
+  // console.log(`ini index ke ${i} dari contohArray yaitu : ${contohArray[i]}`)
+}
+
+// 2. contoh mainan object
+const contohObj = {
+  name: "imam",
+  age: 28,
+  id: 1,
+};
+
+// console.log(contohObj.name)
+
+for (let i = 0; i < contohArray.length; i++) {
+  if (contohArray[i] === "Ale") {
+    console.log(`ini index ke ${i} dari contohArray yaitu : ${contohArray[i]}`);
+  }
+}
+
+contohObj.id = contohArray[0];
+contohObj.name = contohArray[1];
+contohObj.age = contohArray[2];
+
+// 1 :
+// print contohObj dengan key name ambil dari index 2 contohArray
+console.log(`Key name: ${contohObj.name}`);
+// print contohObj dengan key age ambil dari index 3 contohArray
+console.log(`Key age: ${contohObj.age}`);
+// print contohObj dengan key id ambil dari index 1 contohArray
+console.log(`Key id: ${contohObj.id}`);
+
+// 2 :
+// - kalian tambah data kalian di arr + 1 temen kalian juga
+contohArray.push(3, "Ranto", 25);
+// - menggunakan looping, di dalam nya check condisi pake if utk print contohObj key id yang value nya dari index 0,3,6
+for (let i = 0; i < contohArray.length; i++) {
+  if (i == 0 || i == 3 || i == 6) {
+    contohObj.id = contohArray[i];
+    console.log(`index ${[i]} berisikan ${contohObj.id}`);
+  }
+}
+
+// 3
+// masukkan data object dengan arr.push (Nirwana)
+// tapi hanya masukkan data yang bernama imam, hafizh, syifa (pake IF dari hafizh no 2) ke dalam array kosong
+// dari array kosong yang udah di masukkan data diatas, tapi cuman print yang age nya dibawah 28 dan id nya  (loping dan if) 2
+let data = [];
+
+const obj1 = { name: "Imam", age: 28, id: 1 };
+const obj2 = { name: "Hafizh", age: 20, id: 2 };
+const obj4 = { name: "Ale", age: 22, id: 3 };
+const obj5 = { name: "Dimas", age: 23, id: 4 };
+const obj6 = { name: "Adella", age: 25, id: 5 };
+const obj7 = { name: "Syifa", age: 21, id: 6 };
+
+// arr of object
+// arr = [{ name: 'Imam', age: 28, id: 1 }]
+
+const arr = [obj1, obj2, obj4, obj5, obj6, obj7];
+
+for (let i = 0; i < arr.length; i++) {
+  if (
+    (arr[i].name === "Imam" ||
+      arr[i].name === "Hafizh" ||
+      arr[i].name === "Syifa") &&
+    arr[i].age < 28 &&
+    arr[i].id === 2
+  ) {
+    data.push(arr[i]);
+  }
+}
+
+for (let i = 0; i < data.length; i++) {
+  console.log(`Name: ${data[i].name}, Age: ${data[i].age}, ID: ${data[i].id}`);
+}
